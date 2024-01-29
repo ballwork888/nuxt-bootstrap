c7189b823625b3342f1482ebd16e4f36a7a8cf4a
New <aio4t9@protonmail.com>
2024-01-29 13:22:42 +0700

    Update result.vue

--- pages/lottery/result.vue
+++ pages/lottery/result.vue
@@ -11,21 +11,21 @@
         <hr>
 
         <div class="row">
-            <div class="col-6">
+            <div class="col-12 col-lg-6">
                 <LotteryResult3 title="หวยรัฐบาล 28 มกราคม 2567"></LotteryResult3>
             </div>
-            <div class="col-6">
+            <div class="col-12 col-lg-6">
                 <LotteryResult2 title="หวย ธกส. 28 มกราคม 2567"></LotteryResult2>
             </div>
         </div>
         <div class="row">
-            <div class="col-4">
+            <div class="col-12 col-lg-4">
                 <LotteryResult2 title="หวย ออมสิน 28 มกราคม 2567"></LotteryResult2>
             </div>
-            <div class="col-4">
+            <div class="col-12 col-lg-4">
                 <LotteryResult2 title="หวย ออมสิน 28 มกราคม 2567"></LotteryResult2>
             </div>
-            <div class="col-4">
+            <div class="col-12 col-lg-4">
                 <LotteryResult2 title="หวย ออมสิน 28 มกราคม 2567"></LotteryResult2>
             </div>
         </div>
